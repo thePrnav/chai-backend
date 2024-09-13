7615ffe1c927d5a1318183a9fb8f7155fee93c5d
thePrnav <pranavsalunkhe448@gmail.com>
2024-09-13 16:06:27 +0530

    using multer to upload file in backend

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)     // after complete project change originalname
+    }
+  })
+  
+  export const upload = multer({ storage, })
