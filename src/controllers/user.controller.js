36764ee98677684d083a9e82eac11bb819944c08
thePrnav <pranavsalunkhe448@gmail.com>
2024-09-18 23:42:13 +0530

    Access Refresh Token, Middleware and cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        user.save({ValidateBeforeSave: false})
+
+        return {accessToken, refreshToken}
+        
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access and refresh token")
+    }
+}
 
 const registerUser = asyncHandler(async(req,res) => {
 
@@ -94,4 +109,92 @@ const registerUser = asyncHandler(async(req,res) => {
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async(req,res) => {
+
+//_______________________ Algo ______________________________________________
+    
+    // Login User :- step by step process (Logic Building) 
+
+    // 1) req body -> data 
+    // 2) username or email 
+    // 3) find the user
+    // 4) check password
+    // 5) access & refresh token
+    // 6) send cookie
+     
+//_______________________________________________________________________________________
+
+   const {username, email , password} = req.body
+
+   if(!username || !email){
+          throw new ApiError(400, "username or email is required")
+   }
+
+   const user = await User.findOne({
+    $or: [{username}, {email}]
+   })
+
+   if(!user){
+        throw new ApiError(404, "User does not exist")
+   }
+
+   const isPasswordvalid = await user.isPasswordCorrect(password)
+
+   if(!isPasswordvalid){
+    throw new ApiError(401, "Invalid user credentials")
+   }
+
+   const {refreshToken, accessToken} = await generateAccessAndRefreshTokens(user._id)
+
+   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+   const options = {                     // ye cookies default modifiable hoti hai toh koi bhi access kar sakta hai
+    httpOnly: true,                      //  jab (httpOnly : true, secure: true) karte ho tab cookies server se hi modifiable hoti hai
+    secure: true
+   }
+
+   return res
+   .cookie("accessToken", accessToken, options)
+   .cookie("refreshToken", refreshToken, options)
+   .json(
+    new ApiResponse (
+        200, 
+        {
+            user: loggedInUser, accessToken, refreshToken
+        },
+        "User logged in successfully"
+    )
+   )
+
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+       await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+       )
+
+       const options = {
+        httpOnly: true,
+        secure: true
+       }
+
+       return res
+       .status(200)
+       .clearCookie("accessToken", options)
+       .clearCookie("refreshToken", options)
+       .json(new ApiResponse(200, {}, "User logged out"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 
-import {upload} from "../middlewares/multer.middleware.js"
+import {upload} from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -18,6 +19,12 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 
 export default router
