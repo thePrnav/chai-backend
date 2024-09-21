be93e2829a5d48a60c248415584d3e880a770987
thePrnav <pranavsalunkhe448@gmail.com>
2024-09-21 21:46:11 +0530

    Learn Mongodb aggregation pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import { Subscription } from "../models/subscription.model.js";
 
 const generateAccessAndRefreshTokens = async(userId) => {
     try {
@@ -321,6 +322,8 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
         throw new ApiError(400, "Error while uploading on avatar")
     }
 
+    // TODO: delete old image - Assignment
+
     const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
@@ -369,6 +372,79 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
     )
 })
 
+
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+     const {username} = req.params;
+     if(!username?.trim()){
+        throw new ApiError(400, "Username is missing")
+     }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        }, 
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"              // mere subscriber kitne hai (follower)
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"                      // maine kisko subscribed kar rakha hai (following)
+            },
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"               // $subscribers  - field hai isliye " $ " lagaya hai
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"           
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id , "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,               
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+
+
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -378,5 +454,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
